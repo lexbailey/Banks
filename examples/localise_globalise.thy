032d07064af31d61a2dccfc60dd4a10201b430af
Lex Bailey <danieljabailey@gmail.com>
2023-02-08 15:22:13 +0000

    okay, the errors in this example were obvious in hindsight

--- examples/localise_globalise.thy
+++ examples/localise_globalise.thy
@@ -28,53 +28,45 @@ alphabet v3_view_vars =
 
 definition V3 where "V3 = (vu:d3 = sys:x \<and> (if sys:x < sys:y then (vu:e3 = 0) else vu:e3 = 1))\<^sub>e"
 
-definition ExSys where "ExSys = (x\<^sup>> \<ge> 0 \<and> y\<^sup>> \<ge> 0 \<and> x\<^sup>> + y\<^sup>> = 10)\<^sub>e"
+definition ExSys where "ExSys = \<Delta> (x \<ge> 0 \<and> y \<ge> 0 \<and> x + y = 10)\<^sub>e"
 
-lemma v1_healthy1: "V1 is VH1"
+lemma v1_healthy1: "V1 is VH"
   by (pred_auto_banks add: V1_def)
 
-lemma v2_healthy1: "V2 is VH1"
+lemma v2_healthy1: "V2 is VH"
   by (pred_auto_banks add: V2_def)
 
-lemma v3_healthy1: "V3 is VH1"
+lemma v3_healthy1: "V3 is VH"
   by (pred_auto_banks add: V3_def)
 
 definition Loc1 where "Loc1 = L V1 ExSys"
 
-lemma localise_v1 : "Loc1 = (a1 \<ge> 0 \<and> b1 \<ge> 0 \<and> a1 + b1 = 10)\<^sub>e \<up> vu\<^sup>>"
+lemma localise_v1 : "Loc1 = \<Delta>((a1 \<ge> 0 \<and> b1 \<ge> 0 \<and> a1 + b1 = 10)\<^sub>e \<up> vu)"
   by (pred_auto_banks add: Loc1_def V1_def ExSys_def)
 
 definition Loc2 where "Loc2 = L V2 ExSys"
 
-lemma localise_v2 : "Loc2 = (c2 \<ge> 5 \<and> c2 \<le> 10)\<^sub>e \<up> vu\<^sup>>"
+lemma localise_v2 : "Loc2 = \<Delta>((c2 \<ge> 5 \<and> c2 \<le> 10)\<^sub>e \<up> vu)"
   apply (pred_auto_banks add: Loc2_def V2_def ExSys_def)
   by presburger+
 
 definition Loc3 where "Loc3 = (L V3 ExSys)"
 
-lemma localise_v3 : "Loc3 = (
+lemma localise_v3 : "Loc3 = \<Delta>((
     (d3 \<ge> 0 \<and> d3 < 5 \<and> e3 = 0)
    \<or>(d3 \<ge> 5 \<and> d3 \<le> 10 \<and> e3 = 1)
-  )\<^sub>e \<up> vu\<^sup>>"
+  )\<^sub>e \<up> vu)"
   apply (pred_auto_banks add: Loc3_def V3_def ExSys_def)
-  tr
-
-lemma localise_v3 : "Loc3 = (
-    (d3 \<ge> 0 \<and> d3 < 5 \<and> e3 = 0)
-   \<or>(d3 \<ge> 5 \<and> d3 \<le> 10 \<and> e3 = 1)
-  )\<^sub>e \<up> vu\<^sup>>"
-  apply (pred_auto_banks add: Loc3_def V3_def ExSys_def)
-  tr
   by presburger+
-
-lemma "(G V1 Loc1) =  (x \<ge> 0 \<and> y \<ge> 0 \<and> x + y = 10)\<^sub>e \<up> sys\<^sup>>"
+  
+lemma "(G V1 Loc1) =  \<Delta>((x \<ge> 0 \<and> y \<ge> 0 \<and> x + y = 10)\<^sub>e \<up> sys)"
   by (pred_auto_banks add: Loc1_def V1_def ExSys_def)
 
-lemma "(G V2 Loc2) =  ((max x y \<ge> 5) \<and> (max x y \<le> 10))\<^sub>e  \<up> sys\<^sup>>"
+lemma "(G V2 Loc2) =  \<Delta>(((max x y \<ge> 5) \<and> (max x y \<le> 10))\<^sub>e  \<up> sys)"
   apply (pred_auto_banks add: Loc2_def V2_def ExSys_def)
   by presburger
 
-lemma "(G V3 Loc3) =  ( if x < y then (x \<ge> 0 \<and> x < 5) else (x \<ge> 5 \<and> x \<le> 10))\<^sub>e  \<up> sys\<^sup>>"
+lemma "(G V3 Loc3) =  \<Delta>(( if x < y then (x \<ge> 0 \<and> x < 5) else (x \<ge> 5 \<and> x \<le> 10))\<^sub>e  \<up> sys)"
   apply (pred_auto_banks add: Loc3_def V3_def ExSys_def)
   by presburger+
 
