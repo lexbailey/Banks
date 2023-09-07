62d6d175fdd58d1b9878609d6c37d0dfbc6df6db
Lex Bailey <danieljabailey@gmail.com>
2023-09-07 04:44:40 +0100

    make examples much neater

--- examples/user_pass.thy
+++ examples/user_pass.thy
@@ -3,73 +3,110 @@ theory user_pass
   (*CI_ONLY imports Main "Banks.banks" CI_ONLY*)
 begin
 
-datatype Msg = Success | ErrNoSuchUser | ErrWrongPassword
+datatype Result = Success | Failure
 
+(* The alphabet of the authentication system *)
 alphabet login =
   db :: "string \<Rightarrow> string option"
   username :: string
   password :: string
-  result :: Msg
+  result :: Result
+  message :: string
 
+(* Definition of authentication for success or failure *)
+definition auth where "auth adb user pass =
+  (case (adb user) of
+    None \<Rightarrow> Failure
+    | Some(p) \<Rightarrow> if p = pass then Success else Failure
+  )
+"
+
+definition apply_f where "apply_f a b = a b" 
+
+(* Specification requires that the result variable accurately describes the result of the authentication, but does not specify the message at all *)
+definition spec where "spec = (
+  username\<^sup>> = username\<^sup>< \<and>
+  password\<^sup>> = password\<^sup>< \<and>
+  db\<^sup>> = db\<^sup>< \<and>
+  (
+    (apply_f (db\<^sup><) (username\<^sup><) = None \<and> result\<^sup>> = Failure)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><) \<and> result\<^sup>> = Success)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) \<noteq> Some(password\<^sup><) \<and> apply_f (db\<^sup><) (username\<^sup><) \<noteq> None \<and> result\<^sup>> = Failure)
+  )
+  )\<^sub>e"
+
+(* The attacker's view of the system *)
 alphabet view\<^sub>\<alpha> =
   v_username :: string
   v_password :: string
-  v_result :: Msg
-
-definition auth where "auth adb user pass =
-  (case (adb user) of
-    None \<Rightarrow> ErrNoSuchUser
-    | Some(p) \<Rightarrow> if p = pass then Success else ErrWrongPassword
-  )
-"
+  v_result :: Result
+  v_message :: string
 
 definition system where "system = (
   username\<^sup>> = username\<^sup>< \<and>
   password\<^sup>> = password\<^sup>< \<and>
   db\<^sup>> = db\<^sup>< \<and>
-  result\<^sup>> = (
-    auth (db\<^sup><) (username\<^sup><) (password\<^sup><)
-  ))\<^sub>e"
+  (
+    (apply_f (db\<^sup><) (username\<^sup><) = None \<and> result\<^sup>> = Failure)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><) \<and> result\<^sup>> = Success)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) \<noteq> Some(password\<^sup><) \<and> apply_f (db\<^sup><) (username\<^sup><) \<noteq> None \<and> result\<^sup>> = Failure)
+  ) \<and>
+  (
+    (apply_f (db\<^sup><) (username\<^sup><) = None \<and> message\<^sup>> = ''No such user'')
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><) \<and> message\<^sup>> = ''Success'')
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) \<noteq> Some(password\<^sup><) \<and> apply_f (db\<^sup><) (username\<^sup><) \<noteq> None \<and> message\<^sup>> = ''Incorrect password'')
+  )
+  )\<^sub>e"
 
-definition apply_f where "apply_f a b = a b" 
+(* Proof automation confirms that the spec is refined by the system *)
+lemma "spec \<sqsubseteq> system"
+  by (pred_auto add: spec_def system_def)
 
+(* Attacker can provide a username and password and see the result and message *)
 definition view where "view = (
   vu:v_username = sys:username \<and>
   vu:v_password = sys:password \<and>
-  vu:v_result = sys:result
+  vu:v_result = sys:result \<and>
+  vu:v_message = sys:message
 )\<^sub>e"
-  
+
+(* We can project the system through the view to get a predicate that encodes all the behaviour we can inspect in terms of view variables only *)
 lemma "(L view system) \<down> vu\<^sup>2 = (
     \<exists> db .
       v_username\<^sup>> = v_username\<^sup>< \<and>
       v_password\<^sup>> = v_password\<^sup>< \<and>
-      v_result\<^sup>> = auth db (v_username\<^sup><) (v_password\<^sup><)
+      (
+        (apply_f db (v_username\<^sup><) = None \<and> v_result\<^sup>> = Failure)
+        \<or>(apply_f db (v_username\<^sup><) = Some(v_password\<^sup><) \<and> v_result\<^sup>> = Success)
+        \<or>(apply_f db (v_username\<^sup><) \<noteq> Some(v_password\<^sup><) \<and> apply_f db (v_username\<^sup><) \<noteq> None \<and> v_result\<^sup>> = Failure)
+      ) \<and>
+      (
+        (apply_f db (v_username\<^sup><) = None \<and> v_message\<^sup>> = ''No such user'')
+        \<or>(apply_f db (v_username\<^sup><) = Some(v_password\<^sup><) \<and> v_message\<^sup>> = ''Success'')
+        \<or>(apply_f db (v_username\<^sup><) \<noteq> Some(v_password\<^sup><) \<and> apply_f db (v_username\<^sup><) \<noteq> None \<and> v_message\<^sup>> = ''Incorrect password'')
+      )
   )\<^sub>e"
-  by (pred_auto_banks add: view_def system_def)  
+  by (pred_auto_banks add: view_def system_def)
 
-lemma "(infer (system \<up> sys\<^sup>2) view (vu:v_result\<^sup>> = Success)\<^sub>e) = (
-    sys:username\<^sup>> = sys:username\<^sup>< \<and>
-    sys:password\<^sup>> = sys:password\<^sup>< \<and>
-    sys:db\<^sup>> = sys:db\<^sup>< \<and>
-    sys:result\<^sup>> = Success \<and>
-    (auth (sys:db\<^sup>>) (sys:username\<^sup>>) (sys:password\<^sup>>) = Success)
+lemma "(infer (system \<up> sys\<^sup>2) view (vu:v_message\<^sup>> = ''Success'')\<^sub>e) \<down> sys\<^sup>2 = (
+    username\<^sup>> = username\<^sup>< \<and>
+    password\<^sup>> = password\<^sup>< \<and>
+    db\<^sup>> = db\<^sup>< \<and>
+    message\<^sup>> = ''Success'' \<and>
+    result\<^sup>> = Success \<and>
+    (apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><))
   )\<^sub>e"
-  apply (pred_auto_banks add: view_def system_def)
-  by presburger+
+  by (pred_auto_banks add: view_def system_def)
 
-lemma "(infer (system \<up> sys\<^sup>2) view ((vu:v_username\<^sup>< = ''Alice'') \<and> vu:v_result\<^sup>> = ErrWrongPassword)\<^sub>e) = (
-    sys:username\<^sup>> = ''Alice'' \<and>
-    sys:username\<^sup>< = ''Alice'' \<and>
-    sys:password\<^sup>> = sys:password\<^sup>< \<and>
-    sys:db\<^sup>> = sys:db\<^sup>< \<and>
-    sys:result\<^sup>> = ErrWrongPassword \<and>
-    (auth (sys:db\<^sup>>) ''Alice'' (sys:password\<^sup>>) = ErrWrongPassword) \<and>
-    (apply_f (sys:db\<^sup>>) ''Alice'' \<noteq> None)
+lemma "(infer (system \<up> sys\<^sup>2) view ((vu:v_username\<^sup>< = ''Alice'') \<and> vu:v_message\<^sup>> = ''Incorrect password'')\<^sub>e) \<down> sys\<^sup>2 = (
+    username\<^sup>> = ''Alice'' \<and> username\<^sup>< = ''Alice'' \<and>
+    password\<^sup>> = password\<^sup>< \<and>
+    db\<^sup>> = db\<^sup>< \<and>
+    message\<^sup>> = ''Incorrect password'' \<and>
+    result\<^sup>> = Failure \<and>
+    (apply_f (db\<^sup><) ''Alice'' \<noteq> None) \<and>
+    (apply_f (db\<^sup><) ''Alice'' \<noteq> Some(password\<^sup><))
   )\<^sub>e"
-  apply (pred_auto_banks add: view_def system_def)
-  apply presburger+
-   apply (simp add: auth_def)
-  apply (smt (z3) Msg.distinct(6) apply_f_def option.case(1) option.exhaust)
-  by presburger
+  by (pred_auto_banks add: view_def system_def)
 
 end
--- examples/user_pass_noleak.thy
+++ examples/user_pass_noleak.thy
@@ -3,66 +3,99 @@ theory user_pass_noleak
   (*CI_ONLY imports Main "Banks.banks" CI_ONLY*)
 begin
 
-datatype Msg = Success | ErrBadUsernameOrPassword
+datatype Result = Success | Failure
 
+(* The alphabet of the authentication system *)
 alphabet login =
   db :: "string \<Rightarrow> string option"
   username :: string
   password :: string
-  result :: Msg
+  result :: Result
+  message :: string
 
+definition apply_f where "apply_f a b = a b" 
+
+(* Specification requires that the result variable accurately describes the result of the authentication, but does not specify the message at all *)
+definition spec where "spec = (
+  username\<^sup>> = username\<^sup>< \<and>
+  password\<^sup>> = password\<^sup>< \<and>
+  db\<^sup>> = db\<^sup>< \<and>
+  (
+    (apply_f (db\<^sup><) (username\<^sup><) = None \<and> result\<^sup>> = Failure)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><) \<and> result\<^sup>> = Success)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) \<noteq> Some(password\<^sup><) \<and> apply_f (db\<^sup><) (username\<^sup><) \<noteq> None \<and> result\<^sup>> = Failure)
+  )
+  )\<^sub>e"
+
+(* The attacker's view of the system *)
 alphabet view\<^sub>\<alpha> =
   v_username :: string
   v_password :: string
-  v_result :: Msg
-
-definition view where "view = (
-  vu:v_username = sys:username \<and>
-  vu:v_password = sys:password \<and>
-  vu:v_result = sys:result)\<^sub>e"
-
-definition auth where "auth adb user pass =
-  (case (adb user) of
-    None \<Rightarrow> ErrBadUsernameOrPassword
-    | Some(p) \<Rightarrow> if p = pass then Success else ErrBadUsernameOrPassword
-  )
-"
+  v_result :: Result
+  v_message :: string
 
 definition system where "system = (
   username\<^sup>> = username\<^sup>< \<and>
   password\<^sup>> = password\<^sup>< \<and>
   db\<^sup>> = db\<^sup>< \<and>
-  result\<^sup>> = (
-    auth (db\<^sup><) (username\<^sup><) (password\<^sup><)
-  ))\<^sub>e"
+  (
+    (apply_f (db\<^sup><) (username\<^sup><) = None \<and> result\<^sup>> = Failure)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><) \<and> result\<^sup>> = Success)
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) \<noteq> Some(password\<^sup><) \<and> apply_f (db\<^sup><) (username\<^sup><) \<noteq> None \<and> result\<^sup>> = Failure)
+  ) \<and>
+  (
+    (apply_f (db\<^sup><) (username\<^sup><) = None \<and> message\<^sup>> = ''Authentication failed'')
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><) \<and> message\<^sup>> = ''Success'')
+    \<or>(apply_f (db\<^sup><) (username\<^sup><) \<noteq> Some(password\<^sup><) \<and> apply_f (db\<^sup><) (username\<^sup><) \<noteq> None \<and> message\<^sup>> = ''Authentication failed'')
+  )
+  )\<^sub>e"
 
+(* Proof automation confirms that the spec is refined by the system *)
+lemma "spec \<sqsubseteq> system"
+  by (pred_auto add: spec_def system_def)
+
+(* Attacker can provide a username and password and see the result and message *)
+definition view where "view = (
+  vu:v_username = sys:username \<and>
+  vu:v_password = sys:password \<and>
+  vu:v_result = sys:result \<and>
+  vu:v_message = sys:message
+)\<^sub>e"
+  
 lemma "(L view system) \<down> vu\<^sup>2 = (
-    \<exists> adb .
+    \<exists> db .
       v_username\<^sup>> = v_username\<^sup>< \<and>
       v_password\<^sup>> = v_password\<^sup>< \<and>
-      v_result\<^sup>> = auth adb (v_username\<^sup><) (v_password\<^sup><)
+      (
+        (apply_f db (v_username\<^sup><) = None \<and> v_result\<^sup>> = Failure)
+        \<or>(apply_f db (v_username\<^sup><) = Some(v_password\<^sup><) \<and> v_result\<^sup>> = Success)
+        \<or>(apply_f db (v_username\<^sup><) \<noteq> Some(v_password\<^sup><) \<and> apply_f db (v_username\<^sup><) \<noteq> None \<and> v_result\<^sup>> = Failure)
+      ) \<and>
+      (
+        (apply_f db (v_username\<^sup><) = None \<and> v_message\<^sup>> = ''Authentication failed'')
+        \<or>(apply_f db (v_username\<^sup><) = Some(v_password\<^sup><) \<and> v_message\<^sup>> = ''Success'')
+        \<or>(apply_f db (v_username\<^sup><) \<noteq> Some(v_password\<^sup><) \<and> apply_f db (v_username\<^sup><) \<noteq> None \<and> v_message\<^sup>> = ''Authentication failed'')
+      )
   )\<^sub>e"
-  by (pred_auto_banks add: view_def system_def)  
+  by (pred_auto_banks add: view_def system_def)
 
-lemma "(infer (system \<up> sys\<^sup>2) view (vu:v_result\<^sup>> = Success)\<^sub>e) = (
-    sys:username\<^sup>> = sys:username\<^sup>< \<and>
-    sys:password\<^sup>> = sys:password\<^sup>< \<and>
-    sys:db\<^sup>> = sys:db\<^sup>< \<and>
-    sys:result\<^sup>> = Success \<and>
-    (auth (sys:db\<^sup>>) (sys:username\<^sup>>) (sys:password\<^sup>>) = Success)
+lemma "(infer (system \<up> sys\<^sup>2) view (vu:v_message\<^sup>> = ''Success'')\<^sub>e) \<down> sys\<^sup>2 = (
+    username\<^sup>> = username\<^sup>< \<and>
+    password\<^sup>> = password\<^sup>< \<and>
+    db\<^sup>> = db\<^sup>< \<and>
+    message\<^sup>> = ''Success'' \<and>
+    result\<^sup>> = Success \<and>
+    (apply_f (db\<^sup><) (username\<^sup><) = Some(password\<^sup><))
   )\<^sub>e"
-  apply (pred_auto_banks add: view_def system_def)
-  by presburger+
+  by (pred_auto_banks add: view_def system_def)
 
-lemma "(infer (system \<up> sys\<^sup>2) view ((vu:v_username\<^sup>< = ''Alice'') \<and> vu:v_result\<^sup>> = ErrBadUsernameOrPassword)\<^sub>e) = (
-    sys:username\<^sup>> = ''Alice'' \<and>
-    sys:username\<^sup>< = ''Alice'' \<and>
-    sys:password\<^sup>> = sys:password\<^sup>< \<and>
-    sys:db\<^sup>> = sys:db\<^sup>< \<and>
-    sys:result\<^sup>> = ErrBadUsernameOrPassword \<and>
-    (auth (sys:db\<^sup>>) ''Alice'' (sys:password\<^sup>>) = ErrBadUsernameOrPassword)
+lemma "(infer (system \<up> sys\<^sup>2) view ((vu:v_username\<^sup>< = ''Alice'') \<and> vu:v_message\<^sup>> = ''Authentication failed'')\<^sub>e)  \<down> sys\<^sup>2 = (
+    username\<^sup>> = ''Alice'' \<and> username\<^sup>< = ''Alice'' \<and>
+    password\<^sup>> = password\<^sup>< \<and>
+    db\<^sup>> = db\<^sup>< \<and>
+    message\<^sup>> = ''Authentication failed'' \<and>
+    result\<^sup>> = Failure \<and>
+    (apply_f (db\<^sup><) ''Alice'' \<noteq> Some(password\<^sup><)) 
   )\<^sub>e"
-  apply (pred_auto_banks add: view_def system_def)
-  by presburger+
-
+  by (pred_auto_banks add: view_def system_def)
 end
